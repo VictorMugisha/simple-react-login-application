1c7aeeaee683c923b297168b0d5b5e867573b670
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-22 12:53:10 +0200

    Implemented basic structure of Dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <section>
+      <div className="users-data">
+        <h2 className="header">Welcome back Victor!</h2>
+        <div className="details">
+          <div className="detail">
+            <p>First Name:</p>
+            <h3>Victor</h3>
+          </div>
+          <div className="detail">
+            <p>Last Name:</p>
+            <h3>Mugisha</h3>
+          </div>
+          <div className="detail">
+            <p>Username:</p>
+            <h3>VictorMugisha</h3>
+          </div>
+          <div className="detail">
+            <p>Remember Me:</p>
+            <h3>Yes</h3>
+          </div>
+          <div className="detail">
+            <p>First Name:</p>
+            <h3>Victor</h3>
+          </div>
+        </div>
+      </div>
+      <button className="logout-button">Logout</button>
+    </section>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import Dashboard from "./Dashboard"
 
 const Signup = () => {
     const [signupData, setSignupData] = useState({
+        id: '',
         firstName: '',
         lastName: '',
         username: '',
         password: '',
         repeatPassword: '',
-        rememberMe: false
+        rememberMe: false,
+        isLoggedIn: false
     });
+
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
 
     const [users, setUsers] = useState(() => {
         // Initializing the users state with data from localStorage, if available
@@ -39,11 +45,13 @@ const Signup = () => {
         }
 
         const realData = {
+            id: nanoid(),
             firstName,
             lastName,
             username,
             password,
-            rememberMe
+            rememberMe,
+            isLoggedIn: true
         };
 
         // Retrieving stored users and check for possible existing username
@@ -55,80 +63,90 @@ const Signup = () => {
         }
 
         // Updating state and localStorage
-        const updatedUsers = [...usersStored, realData];
+        const updatedUsers = [realData, ...usersStored];
         setUsers(updatedUsers);
         localStorage.setItem('usersData', JSON.stringify(updatedUsers));
+
+
+        // Log user in
+        setIsUserLoggedIn(true)
     }
 
     return (
         <section>
-            <div className="header">
-                <h2>Create Your Account</h2>
-            </div>
-            <form className="form-container" onSubmit={handleSubmit}>
-                <div className="input">
-                    <p>First Name</p>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        name="firstName"
-                        value={signupData.firstName}
-                        onChange={handleChange}
-                    />
+            {
+                isUserLoggedIn ?
+                <Dashboard user={users[0]} logoutSession={setIsUserLoggedIn} /> :
+                <div className="signup-form">
+                    <div className="header">
+                        <h2>Create Your Account</h2>
+                    </div>
+                    <form className="form-container" onSubmit={handleSubmit}>
+                        <div className="input">
+                            <p>First Name</p>
+                            <input
+                                type="text"
+                                placeholder="First Name"
+                                name="firstName"
+                                value={signupData.firstName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input">
+                            <p>Last Name</p>
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                name="lastName"
+                                value={signupData.lastName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input">
+                            <p>Username</p>
+                            <input
+                                type="text"
+                                placeholder="Create Your Username"
+                                name="username"
+                                value={signupData.username}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input">
+                            <p>Password</p>
+                            <input
+                                type="password"
+                                placeholder="Enter Your Password"
+                                name="password"
+                                value={signupData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input">
+                            <p>Confirm Password</p>
+                            <input
+                                type="password"
+                                placeholder="Repeat Password"
+                                name="repeatPassword"
+                                value={signupData.repeatPassword}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input flex-center">
+                            <input
+                                type="checkbox"
+                                name="rememberMe"
+                                checked={signupData.rememberMe}
+                                onChange={handleChange}
+                            />
+                            <p>Remember Me</p>
+                        </div>
+                        <div>
+                            <button className="signup-btn">Create Account</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="input">
-                    <p>Last Name</p>
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        name="lastName"
-                        value={signupData.lastName}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="input">
-                    <p>Username</p>
-                    <input
-                        type="text"
-                        placeholder="Create Your Username"
-                        name="username"
-                        value={signupData.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="input">
-                    <p>Password</p>
-                    <input
-                        type="password"
-                        placeholder="Enter Your Password"
-                        name="password"
-                        value={signupData.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="input">
-                    <p>Confirm Password</p>
-                    <input
-                        type="password"
-                        placeholder="Repeat Password"
-                        name="repeatPassword"
-                        value={signupData.repeatPassword}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="input flex-center">
-                    <input
-                        type="checkbox"
-                        name="rememberMe"
-                        checked={signupData.rememberMe}
-                        onChange={handleChange}
-                    />
-                    <p>Remember Me</p>
-                </div>
-                <div>
-                    <button className="signup-btn">Create Account</button>
-                </div>
-            </form>
+            }
         </section>
     );
 }
